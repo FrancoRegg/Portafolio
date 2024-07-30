cee327a612ce703960dced4f8b51731ec686e467
FrancoRegg <francoreggiardo97@gmail.com>
2024-07-30 19:15:40 +0200

    Cambio idioma

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,10 +13,10 @@ function Projects() {
     <Container fluid className="project-section">
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Mis <strong className="purple">Trabajos </strong> Recientes
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+        Aquí hay algunos proyectos en los que he trabajado recientemente.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
