1fc9f4217af56f699b703bb64cd0dcb96a61af5a
FrancoRegg <francoreggiardo97@gmail.com>
2024-07-31 18:00:39 +0200

    Nuevo proyecto

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,8 @@ import ProjectCard from "./ProjectCards";
 //import leaf from "../../Assets/Projects/leaf.png";
 //import emotion from "../../Assets/Projects/emotion.png";
 //import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+//import chatify from "../../Assets/Projects/chatify.png";
+import palante from "../../Assets/Projects/palante.png";
 //import suicide from "../../Assets/Projects/suicide.png";
 //import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -21,12 +22,11 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={palante}
               isBlog={false}
-              title="Chatify"
+              title="Pa'lante"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/FrancoRegg/Pa-lante"
             />
           </Col>
 
