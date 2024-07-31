5d4b0c783fce4b556b31cb6a105a9967c9e88f94
FrancoRegg <francoreggiardo97@gmail.com>
2024-07-31 19:21:54 +0200

    Cambio el orden de los componentes

--- src/App.js
+++ src/App.js
@@ -34,16 +34,16 @@ function App() {
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
-        {/*<Home />
+        <Home />
+        <Projects />
         <About />
-        <Projects />*/}
-        <Routes>
+        {/*<Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           {/*<Route path="/resume" element={<Resume />} />}
-          <Route path="*" element={<Navigate to="/"/>} />*/}
-        </Routes>
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>*/}
         <Footer />
       </div>
     </Router>
