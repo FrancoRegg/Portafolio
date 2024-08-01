c02e1151cb1c2e9ad16d1401bdc72ce2e3e2ea4c
FrancoRegg <francoreggiardo97@gmail.com>
2024-08-01 20:06:00 +0200

    Creo una funcion que toma como referencia los componente de la pagina para hacer scroll al interactuar con el navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-//import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -19,6 +13,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
+
+  const homeRef = useRef(null);
+  const projectsRef = useRef(null);
+  const aboutRef = useRef(null);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -28,22 +26,26 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
+        <Navbar
+          scrollToHome={() => scrollToSection(homeRef)}
+          scrollToProjects={() => scrollToSection(projectsRef)}
+          scrollToAbout={() => scrollToSection(aboutRef)}
+        />
         <ScrollToTop />
-        <Home />
-        <Projects />
-        <About />
-        {/*<Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          {/*<Route path="/resume" element={<Resume />} />}
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>*/}
+        <div ref={homeRef}><Home /></div>
+        <div ref={projectsRef}><Projects /></div>
+        <div ref={aboutRef}><About /></div>
         <Footer />
       </div>
     </Router>
@@ -51,3 +53,4 @@ function App() {
 }
 
 export default App;
+
