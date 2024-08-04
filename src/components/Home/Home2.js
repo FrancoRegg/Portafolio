a15f4e30f9c7fe316a442059b99b8d0000ada82d
FrancoRegg <francoreggiardo97@gmail.com>
2024-08-04 10:31:26 +0200

    Actualizacion

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-//import myImg from "../../Assets/avatar.svg";
-//import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
 } from "react-icons/ai";
@@ -14,51 +12,6 @@ function Home2() {
   return (
     <Container fluid className="home-about-section">
       <Container>
-        {/*<Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              ME <span className="yellow"> PRESENTO </span> 
-            </h1>
-            <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="yellow"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="yellow">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="yellow">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="yellow">Node.js</b> and
-              <i>
-                <b className="yellow">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="yellow"> React.js and Next.js</b>
-              </i>
-            </p>
-          </Col>
-          <Col md={4} className="myAvtar">
-            <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
-          </Col>
-        </Row>*/}
         <Row>
           <Col md={12} className="home-about-social">
             <h1>ENCUENTRAME EN</h1>
